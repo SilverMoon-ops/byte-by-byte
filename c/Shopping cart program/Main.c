ecb29eae817b0e89f84af9e117347b3059ab99c0
Kei <scharn746@gmail.com>
2025-11-10 22:28:07 +0530

    write the code and do some testing

--- c/Shopping cart program/Main.c
+++ c/Shopping cart program/Main.c
@@ -4,7 +4,7 @@ int main() {
     
     //Shopping cart program
  
-    char itemName[50] = "";
+    char item[50] = "";
     float price = 0.0f;
     int quantity = 0;
     char currency = '$';
@@ -13,7 +13,16 @@ int main() {
     // i have initialized the variables right here to avoid any undefined behavior
 
     printf("What item would you like to purchase?: ");
+    fgets(item, sizeof(item), stdin);
+
     printf("What is the price for each?: ");
+    scanf("%f", &price);
+
     printf("How many would you like to buy?: ");
+    scanf("%d", &quantity);
+
+    total = price * quantity;
+
+    printf("You have purchased %d of %s at a price of %.2f%c each for a total of %.2f%c\n", quantity, item, price, currency, total, currency);
     return 0;
 }
