510c9fbd579483dde50b08677438e4ac002e0c7c
Kei <scharn746@gmail.com>
2025-11-10 22:19:10 +0530

    Added variable initializations and print statements

---
+++ c/Shopping cart program/Main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    //Shopping cart program
+ 
+    char itemName[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
+
+    // i have initialized the variables right here to avoid any undefined behavior
+
+    printf("What item would you like to purchase?: ");
+    printf("What is the price for each?: ");
+    printf("How many would you like to buy?: ");
+    return 0;
+}
