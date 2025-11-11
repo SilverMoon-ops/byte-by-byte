bd52d1551a43454b3a03c532f7d6629636bf5222
Kei <scharn746@gmail.com>
2025-11-11 11:58:28 +0530

    Update Main.c

--- C/mad_libs/Main.c
+++ C/mad_libs/Main.c
@@ -22,5 +22,13 @@ int main() {
     printf("Enter an adjective (descripition): ");
     fgets(adjective3, sizeof(adjective3), stdin);
 
+    printf("%s\n", noun);
+    printf("%s\n", verb);
+    printf("%s\n", adjective1);
+    printf("%s\n", adjective2);
+    printf("%s\n", adjective3);
+
+    return 0;
+
 
 }
