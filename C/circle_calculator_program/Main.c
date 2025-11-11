0f4e431c6a988a4d17b01ddd120ffee1b7fb6f88
Kei <scharn746@gmail.com>
2025-11-11 13:41:34 +0530

    Declared and initilization variables

---
+++ C/circle_calculator_program/Main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double radius = 0.0;
+    double area = 0.0;
+    const double PI = 3.14159;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%lf", &radius);
+
+    
+
+
+
+
+
+
+    return 0;
+}
