4cade7e14dbe05892bfb51a7e83c007e138f6da7
Kei <scharn746@gmail.com>
2025-11-11 11:40:21 +0530

    Update Main.c

---
+++ C/mad_libs/Main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   
+    // Mad Libs Game
+
+    char noun[50] = "";
+    char verb[50] = "";
+    char adjective1[50] = "";
+    char adjective2[50] = "";
+    char adjective3[50] = "";
+
+    printf("Enter an adjective (descripition): ");
+    printf("Enter a noun (person, place, or thing): ");
+    printf("Enter an adjective (descripition): ");
+    printf("Enter a verb (action word): ");
+    printf("Enter an adjective (descripition): ");
+
+
+}
