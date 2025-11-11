27753f2adf3c6fce7ef42d4c57b44ea9a88da1f2
Kei <scharn746@gmail.com>
2025-11-11 13:47:55 +0530

    Update Main.c

--- C/circle_calculator_program/Main.c
+++ C/circle_calculator_program/Main.c
@@ -5,14 +5,19 @@ int main() {
 
     double radius = 0.0;
     double area = 0.0;
+    double surface_area = 0.0;
     const double PI = 3.14159;
 
     printf("Enter the radius of the circle: ");
     scanf("%lf", &radius);
 
     area = PI * pow(radius, 2); // math.h function to calculate power
-    printf("The area of the circle with radius %.2f is %.2f\n", radius, area);
+    surface_area = 4 * PI * pow(radius, 2); // math.h function to calculate power
 
+
+
+    printf("The area of the circle with radius %.2lf is %.2lf\n", radius, area);
+    printf("The surface area of the sphere with radius %.2lf is %.2lf\n", radius, surface_area);
 
     
 
