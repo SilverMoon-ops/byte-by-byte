4d5690412e607b398fb14f6f331a9f06d7824c39
Kei <scharn746@gmail.com>
2025-11-11 16:47:06 +0530

    made program structure

--- Java/weight_conversion_program/src/Main.java
+++ Java/weight_conversion_program/src/Main.java
@@ -4,10 +4,18 @@ public class Main {
     public static void main(String[] args){
 
 
-        //WEIGHT CONVERSION PROGRAM
+        // WEIGHT CONVERSION PROGRAM
 
+        // Declare variables
 
+        // welcome message
 
+        // prompt for use choice
 
+        // option 1 convert lbs to kgs
+
+        // option 2 convert kgs to lbs
+
+        //else print not a valid choice
     }
 }
