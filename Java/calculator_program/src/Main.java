3a69a8c54e37aa10f2246d34720766c1f0598e70
Kei <scharn746@gmail.com>
2025-11-12 20:02:50 +0530

    Update Main.java

--- Java/calculator_program/src/Main.java
+++ Java/calculator_program/src/Main.java
@@ -5,6 +5,21 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        double num1;
+        double num2;
+        String operator;
+        double result;
+
+        System.out.println("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Enter the operator ( +, -, *, /, ^, %, MOD ");
+        operator = scanner.nextLine();
+
+        System.out.println("Enter the second number: ");
+        num2 = scanner.nextDouble();
+        
+
         scanner.close();
 
     }
