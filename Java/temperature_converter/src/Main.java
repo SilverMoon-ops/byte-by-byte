1f7444ed585b735cc486fda9603757d270fbc05e
Kei <scharn746@gmail.com>
2025-11-12 17:31:47 +0530

    Ternary operatior structure

--- Java/temperature_converter/src/Main.java
+++ Java/temperature_converter/src/Main.java
@@ -5,6 +5,23 @@ public class Main {
 
 
         Scanner scanner = new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.print("Enter the Temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.print("Convert to Celsius or Fahrenheit (C OR F): ");
+        unit = scanner.next().toUpperCase();
+
+        // (Condition) ? True : False
+
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 5 / 9) + 32;
+
+        
+
         scanner.close();
     }
 }
