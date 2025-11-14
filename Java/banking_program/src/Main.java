146175996fc6dcf603ad69b20956a1a55e6aed31
Kei <scharn746@gmail.com>
2025-11-14 18:12:16 +0530

    Update Main.java

--- Java/banking_program/src/Main.java
+++ Java/banking_program/src/Main.java
@@ -5,32 +5,32 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        double balance;
+        double balance = 10.99;
         boolean isRunning = true;
         int choice;
 
-        System.out.println("*****************");
-        System.out.println("BANKING PROGRAM");
-        System.out.println("*****************");
-        System.out.println("1. Show Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Exit");
-        System.out.println("*****************");
+        while (isRunning){
 
-        System.out.println("Enter your choice (1-4): ");
-        choice = scanner.nextInt();
+            System.out.println("*****************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("*****************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("*****************");
 
-        switch (choice){
-            case 1 -> System.out.println(" 1");
-            case 2 -> System.out.println(" 2");
-            case 3 -> System.out.println(" 3");
-            case 4 -> isRunning = false;
-            default -> System.out.println("INVALID!");
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+
+            switch (choice){
+                case 1 -> System.out.println(balance);
+                case 2 -> System.out.println("Deposit");
+                case 3 -> System.out.println("Withdraw");
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID!");
+            }
         }
-
-        // showBalance
-
         // deposit
 
         // withdraw
@@ -38,5 +38,9 @@ public class Main {
         // exit massage
 
         scanner.close();
+        }
+    static void shoeBalance(double balance){
+        System.out.println("*****************");
+        System.out.printf("$%.2f\n", balance);
     }
 }
