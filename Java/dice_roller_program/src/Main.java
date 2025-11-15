e2624880514a02bf50017324f9fa80c8477db823
Kei <scharn746@gmail.com>
2025-11-15 15:44:26 +0530

    new progarm

---
+++ Java/dice_roller_program/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+
+
+        // declare variable
+        // get number of dice from the user
+        //check if number of dice is < 0
+        //get the total
+        // get the ascii art for dice
+    }
+}
