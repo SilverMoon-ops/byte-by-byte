4c4ce23d3ccef20482dda25d0b2b59f4813e137f
Kei <scharn746@gmail.com>
2025-11-17 12:28:39 +0530

    new program quiz_game structue

---
+++ Java/quiz_game/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+        // Question array
+        // Option array
+        // Declare variable
+        // welcome massage
+        // Question loop
+        //     Options
+        //     get guess from the user
+        //     check our guess
+        //Display final score
+    }
+}
