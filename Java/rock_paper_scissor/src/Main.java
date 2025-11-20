6c286c49fb7ad7535622c8cb9de681efbcee6cee
Kei <scharn746@gmail.com>
2025-11-20 16:07:43 +0530

    New exercise

---
+++ Java/rock_paper_scissor/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        // ROCK PAPER SCISSORS GAME
+
+        //DECLARE VARIABLES
+        // GET CHOICE FROM THE USER
+        // GET RANDOM CHOICE FOR THE COMPUTER
+        // CHECK WIN CONDITIONS
+        // ASK TO PLAY AGAIN
+        // GOODBYE MESSAGE
+    }
+}
