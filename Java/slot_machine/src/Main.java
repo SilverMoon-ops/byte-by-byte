8a39c23acb7fc38e5554bb74a4553eca1f04e56d
Kei <scharn746@gmail.com>
2025-11-28 16:19:08 +0530

    rough outline

--- Java/slot_machine/src/Main.java
+++ Java/slot_machine/src/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+    public static void main(String[] args){
+
+        // JAVA SLOT MACHINE
+
+        // DECLARE VARIABLE
+        // DISPLAY WELCOME MASSAGE
+        // PLAY IF BALANCE IS > 0
+        // ENTER BET AMOUNT
+        //     VERIFY IF BET IS > BALANCE
+        //     VERIFY IF BET IS > 0
+        //     SUBTRACT BET FROM BALANCE
+        // SPIN ROW
+        // PRINT ROW
+        // GET PAYOUT
+        // ASK TO PLAY AGAIN
+        // EXIT
+    }
 }
